48bafedea2b95c6530ec47854ed46b28163c0ab5
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2022-10-20 09:11:53 +0200

    fixed drop tables thingy

--- api/sql/createTables.js
+++ api/sql/createTables.js
@@ -14,12 +14,14 @@ const db = mysql.createConnection({
   password: DB_PASSWORD,
   database: DB_DATABASE,
   port: DB_PORT,
+  multipleStatements: true
 });
 
 // CREATE TABLES
 
 db.connect(async (err, connection) => {
   console.log('RUNNING CREATE TABLE SCRIPT');
+  let dropAllTables = `DROP TABLE IF EXISTS UsersWithRoles; DROP TABLE IF EXISTS Roles; DROP TABLE IF EXISTS Users;`;
 
   let createUsersTable = `CREATE TABLE Users (
     userId int NOT NULL AUTO_INCREMENT, 
@@ -43,25 +45,32 @@ db.connect(async (err, connection) => {
     ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
     `;
 
-  db.query(createUsersTable, async (err) => {
+  db.query(dropAllTables, async (err) => {
     if (err) {
-      console.log('ERROR CREATING USERS TABLE', err);
+      console.log('ERROR DROPPING TABLES', err);
       process.exit(1);
     }
-    console.log('USERS TABLE CREATED!');
-    db.query(createRolesTable, async (err) => {
+    console.log("ALL TABLES DROPPED!");
+    db.query(createUsersTable, async (err) => {
       if (err) {
-        console.log('ERROR CREATING ROLES TABLE', err);
+        console.log('ERROR CREATING USERS TABLE', err);
         process.exit(1);
       }
-      console.log('ROLES TABLE CREATED!');
-      db.query(createUsersWithRolesTable, async (err) => {
+      console.log('USERS TABLE CREATED!');
+      db.query(createRolesTable, async (err) => {
         if (err) {
-          console.log('ERROR CREATING USERS WITH ROLES TABLE', err);
+          console.log('ERROR CREATING ROLES TABLE', err);
           process.exit(1);
         }
-        console.log('USERSWITHROLE TABLE CREATED!');
-        process.exit(0);
+        console.log('ROLES TABLE CREATED!');
+        db.query(createUsersWithRolesTable, async (err) => {
+          if (err) {
+            console.log('ERROR CREATING USERS WITH ROLES TABLE', err);
+            process.exit(1);
+          }
+          console.log('USERSWITHROLE TABLE CREATED!');
+          process.exit(0);
+        });
       });
     });
   });
