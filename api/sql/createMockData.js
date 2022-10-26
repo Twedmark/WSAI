1211eee0fc61741551471f6454a3415f7f42be9a
Twedmark <axel.twedmark@hotmail.com>
2022-10-26 13:37:25 +0200

    getting data for product from databas

--- api/sql/createMockData.js
+++ api/sql/createMockData.js
@@ -29,7 +29,8 @@ let mockProduct = {
   description: "Posthorn, från Kalmar Werkstätten, är en minimalistisk pendellampa med en charmigt öglad arm som håller två skärmar i siden. De naturliga lampskärmarna bidrar till ett omgivande ljus i alla riktningar. I 140 år har Kalmar ägnat sig åt anrikt hantverk och progressiv design för att skapa dekorativ och funktionell belysning. Det österrikiska hantverket är elegant i geometrin och delikat i sina proportioner. Kombinationen av ärliga material, många års erfarenhet inom hantverket och modern design, ingjuter lamporna en rigorös minimalism med värme och mänsklighet.",
   material: "Polerad nickel eller mässing, skärm i silke. Svart textilsladd.",
   dimensions: "Bredd: 60 cm, Djup: 18 cm, Lamphöjd: 27,5 cm, Total höjd: max. 180 cm",
-  specification: "Rekommenderad ljuskälla: 2 x E14, 40W. Ljuskälla ingår ej."
+  specification: "Rekommenderad ljuskälla: 2 x E14, 40W. Ljuskälla ingår ej.",
+  images:"[https://artilleriet.centracdn.net/client/dynamic/images/46937_6bd28f389d-kalmar-w-posthorn-pendant-polished-brass-zoom.jpg?w=2000, https://artilleriet.centracdn.net/client/dynamic/images/46930_8f4198bbc3-kalmar-posthorn-pendant-nickel-3-zoom.jpg?w=2000, "
 }
 
 db.connect(async (err, connection) => {
@@ -49,7 +50,7 @@ db.connect(async (err, connection) => {
   let superAdminWithAdminRole = `INSERT INTO UsersWithRoles (userId, roleId) VALUES (3, 2000);`;
   let superAdminWithSuperAdminRole = `INSERT INTO UsersWithRoles (userId, roleId) VALUES (3, 3000);`;
   
-  let products = `INSERT INTO Products (productId, name, price, description, material, dimensions, specification) VALUES (null, "${mockProduct.name}", "${mockProduct.price}", "${mockProduct.description}", "${mockProduct.material}", "${mockProduct.dimensions}", "${mockProduct.specification}");`;
+  let products = `INSERT INTO Products (productId, name, price, description, material, dimensions, specification, images) VALUES (null, "${mockProduct.name}", "${mockProduct.price}", "${mockProduct.description}", "${mockProduct.material}", "${mockProduct.dimensions}", "${mockProduct.specification}", "${mockProduct.images}");`;
 
   let query = userAccount + adminAccount + superAdminAccount + userRole + adminRole + superAdminRole + userWithUserRole + adminWithUserRole + adminWithAdminRole + superAdminWithUserRole + superAdminWithAdminRole + superAdminWithSuperAdminRole + products;
   db.query(query, async (err) => {
--- frontend/pages/product/[id].tsx
+++ frontend/pages/product/[id].tsx
@@ -9,6 +9,7 @@ type ProductProps = {
 	material: string;
 	dimensions: string;
 	specification: string;
+	images: string;
 };
 
 const Product: FC<ProductProps> = ({
@@ -18,12 +19,13 @@ const Product: FC<ProductProps> = ({
 	material,
 	dimensions,
 	specification,
+	images,
 }) => {
 	const router = useRouter();
 	// const { productId } = router.query;
 
 	function addToCart() {
-		console.log("add to cart", name);
+		console.log("add to cart", images);
 		console.log("add to cart");
 	}
 
@@ -50,40 +52,16 @@ const Product: FC<ProductProps> = ({
 				</div>
 				<div className={styles.infoScrollContainer}>
 					<div className={styles.infoContainer}>
-						<h1 className={styles.title}>Posthorn</h1>
-						<p className={styles.description}>
-							Posthorn, från Kalmar Werkstätten, är en minimalistisk pendellampa
-							med en charmigt öglad arm som håller två skärmar i siden. De
-							naturliga lampskärmarna bidrar till ett omgivande ljus i alla
-							riktningar. I 140 år har Kalmar ägnat sig åt anrikt hantverk och
-							progressiv design för att skapa dekorativ och funktionell
-							belysning. Det österrikiska hantverket är elegant i geometrin och
-							delikat i sina proportioner. Kombinationen av ärliga material,
-							många års erfarenhet inom hantverket och modern design, ingjuter
-							lamporna en rigorös minimalism med värme och mänsklighet.
-						</p>
-						<h2>12000 kr</h2>
+						<h1 className={styles.title}>{name}</h1>
+						<p className={styles.description}>{description}</p>
+						<h2>{price}</h2>
 						<button onClick={addToCart}>Add to cart</button>
 					</div>
 				</div>
 			</main>
 			<article className={styles.article}>
-				<h1 className={styles.title}>Product Name</h1>
-				<p className={styles.description}>
-					Some further technical information about the product
-				</p>
-				<p>
-					Material: Polerad nickel eller mässing, skärm i silke. Svart
-					textilsladd.
-				</p>
-				<p>
-					Mått: Bredd: 60 cm Djup: 18 cm Lamphöjd: 27,5 cm Total höjd: max. 180
-					cm
-				</p>
-				<p>
-					Specifikationer: Rekommenderad ljuskälla: 2 x E14, 40W Ljuskälla ingår
-					ej.
-				</p>
+				<h1 className={styles.title}>{name}</h1>
+				<p className={styles.description}>{description}</p>
 			</article>
 		</div>
 	);
@@ -91,7 +69,7 @@ const Product: FC<ProductProps> = ({
 
 export default Product;
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps(context: any) {
 	const productId = context.params.id;
 	const product = await fetch(
 		`http://localhost:4000/getProductById/${productId}`
