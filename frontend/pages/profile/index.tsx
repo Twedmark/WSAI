a8d480d4cd4d2c24ac6083fef1fc90ecccfc1b9b
Twedmark <axel.twedmark@hotmail.com>
2022-11-08 10:16:33 +0100

    13

--- frontend/pages/profile/index.tsx
+++ frontend/pages/profile/index.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
 import React, { useEffect, useState } from "react";
-import { json } from "stream/consumers";
+import styles from "./profile.module.css";
 
 type UserReceipts = {
 	receiptId: number;
@@ -30,13 +30,11 @@ const Profile: NextPage = () => {
 				);
 				if (response.status === 200) {
 					let data = await response.json();
-					// JSON parse all products in data again.products and add it back to data
 					data = data.map((receipt: UserReceipts) => {
 						receipt.products = JSON.parse(receipt.products);
 						return receipt;
 					});
 					console.log(data);
-
 					setReceipts(data);
 				}
 			} catch (err) {
@@ -50,15 +48,46 @@ const Profile: NextPage = () => {
 		fetchUsers();
 	}, []);
 
+	async function fetchProducts(productId) {
+		const response = await fetch(
+			`http://localhost:4000/getProductById/${productId}`
+		);
+		if (response.status === 200) {
+			let data = await response.json();
+			console.log(data);
+			return data;
+		}
+	}
+
 	return (
 		<div>
 			<h1>Profile</h1>
 			<ul>
 				{Receipts.map((receipt: UserReceipts) => (
 					<li key={receipt.receiptId}>
-						<p>Order Nummer: {receipt.receiptId}</p>
-						<p>produkt: {receipt.products[0]}</p>
-						<p>Datum för beställning: {receipt.createdAt}</p>
+						<div>
+							<p className={styles.receiptId}>
+								Order Nummer: {receipt.receiptId}
+							</p>
+							{receipt.products.map((product: any, index) => {
+								return (
+									<section
+										key={index}
+										className={styles.productAndQuantitySection}
+									>
+										<p className={styles.product}>
+											Artikel Nummer: {Object.keys(product)}
+										</p>
+										<p className={styles.Quantity}>
+											Antal: {Object.values(product)}
+										</p>
+									</section>
+								);
+							})}
+							<p className={orderDate}>
+								Datum för beställning: {receipt.createdAt}
+							</p>
+						</div>
 					</li>
 				))}
 			</ul>
